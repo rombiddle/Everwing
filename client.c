8fe43f600ae63dd9563d8f75c4b2a65f365e5ead
Salomé <salome.russier@gmail.com>
2017-07-11 11:44:36 +0200

    ajout monstre et personnage

--- client.c
+++ client.c
@@ -6,14 +6,33 @@
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <time.h>
+#include <stdlib.h>
+
+int const MAX = 1000;
+int const MIN = 100;
+
+struct Personnage{
+    int vie;
+    int attaque;
+};
 
 int main(void) {
+    Personnage p;
+    int random =0;
+
+    srand(time(NULL));
+    random = (rand() % ((MAX-MIN +1)+MIN);
+
+    p.vie = random;
+    p.attaque = random /10;
+
 	printf("Start Client2\n");
 	fflush(stdout);
 
 	int sock, conn;
 	char buffer[100];
-	struct sockaddr_in adr; 
+	struct sockaddr_in adr;
 
 	sock = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -26,9 +45,12 @@ int main(void) {
 
 	conn = connect(sock,(struct sockaddr*) &adr, sizeof(adr));
 
-	write(sock, "Hello",5);
+	// est-il possible de passer un int ?
+	sprintf(buffer, "%d", m.attaque);
+	write(sock, buffer,100);
 		read(sock, buffer,100);
-		printf("message du serveur : %s\n", buffer);
+		m.vie -= atoi(buffer);
+		printf("reste de vie du client: %d\n", m.vie);
 		fflush(stdout);
 
 	// while(1){
--- serveur-multi-test.c
+++ serveur-multi-test.c
@@ -1,5 +1,16 @@
 #include "serveur.h"
 
+int const MAX = 10000;
+int const MIN = 1000;
+
+struct Monstre{
+    int vie;
+    int attaque;
+    //attaque de zone ?
+   // int aoe;
+
+};
+
 void serveurTest(int sockM) {
 	int sock = sockM, client, conn;
 	printf("sock = %d \n", sock);
@@ -11,7 +22,7 @@ void serveurTest(int sockM) {
 	// sock = socket(AF_INET, SOCK_STREAM, 0);
 
 	// bzero(&adr, sizeof(adr));
-	
+
 	// adr.sin_family = AF_INET;
 	// adr.sin_addr.s_addr = htons(INADDR_ANY);
 	// adr.sin_port = htons(7777);
@@ -21,23 +32,32 @@ void serveurTest(int sockM) {
 
 	// //listen(sock, 2);
 	// listen(sock, 10);
-	
+
 	// // printf("%lu\n",sizeof(adr));
 	// // fflush(stdout);
 
 	// clientsize = sizeof(adr);
-	
-	
+
+
 	int x = 0;
 	int all_socket[50];
 	fd_set rdfs;
+
+	Monstre m;
 	int sockmax=sock+1;
+	int random =0;
+
+    srand(time(NULL));
+    random = (rand() % ((MAX-MIN +1)+MIN);
+
+    m.vie = random;
+    m.attaque = random /10;
 
 	printf("Nouveau serveur \n");
 	fflush(stdout);
 
 	while(1){
-		
+
 		// vide tous les sockets qu'on veut ecouter
 		FD_ZERO(& rdfs);
 		FD_SET(sock, &rdfs);
@@ -47,7 +67,7 @@ void serveurTest(int sockM) {
 		for(int i = 0; i < x; i++){
 			FD_SET(all_socket[i], &rdfs);
 		}
-		
+
 
 		// attends pour nouvelle connexion ou qu'il se passe quelquechose sur une connexion actuelle
 		printf("%d Before Select\n", getpid());
@@ -56,8 +76,6 @@ void serveurTest(int sockM) {
 			printf("error select");
 			fflush(stdout);
 		}
-		printf("%d After Select and Before Ajout Client.\n", getpid());
-		fflush(stdout);
 		// ajoute connexion
 		if(FD_ISSET(sock, &rdfs)){
 			printf("isset entered\n");
@@ -69,16 +87,19 @@ void serveurTest(int sockM) {
 			fflush(stdout);
 			x++;
 		}
-		printf("%d After Ajout Client And Before traitement\n", getpid());
-		fflush(stdout);
 		// traitement
 		for(int i = 0; i < x; i++){
 			// == connfd modification lecture
-			conn = read(client, buffer, 100);		
-			printf("%d message récupérer : %s\n", getpid(),buffer);
+			conn = read(client, buffer, 100);
+			//convertie le buffer en Int et me soustrait à la vie
+			m.vie -= atoi(buffer);
+
+			printf("il reste %d vie au monstre \n",m.vie);
 			fflush(stdout);
+			//attauque
+			sprintf(buffer, "%d", m.attaque);
 			write(client,buffer,100);
-			
+
 		}
 
 		printf("%d After traitement\n", getpid());
@@ -89,9 +110,9 @@ void serveurTest(int sockM) {
 			printf("error close");
 			fflush(stdout);
 		}
-		// TODO use shotdown(fd, howto) // mettre flag 
+		// TODO use shotdown(fd, howto) // mettre flag
 	}
 	printf("Adfter While \n");
 	fflush(stdout);
-	
+
 }
